97d68c37ac9d091ed0526d764b4af21f9605a41f
Nanda hafiza <yusronnandahafiza@gmail.com>
2024-01-14 01:05:26 +0700

    add section 2 in dashboard

--- src/components/Dashboard/SectionDua.vue
+++ src/components/Dashboard/SectionDua.vue
@@ -1,3 +1,51 @@
 <template>
-  <h1>section 2</h1>
+  <section class="sec2 container p-5">
+    <div class="row text-center text-white p-5">
+      <h1>Verifikasi Halal Blockchain</h1>
+      <p>Verifikasi Ke-Halalan Blockchain kamu dengan masukkan code atau upload barcode atau QR kamu!</p>
+    </div>
+    <div class="card p-3">
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Masukkan Kode</label>
+          <div class="row">
+            <div class="col-11">
+              <input type="text" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline" />
+            </div>
+            <div class="col-1">
+              <button type="submit" class="btn btn-warning">Submit</button>
+            </div>
+          </div>
+        </div>
+        <div class="form-group pt-2">
+          <label for="exampleInputPassword1">Atau Upload Barcode / QR</label>
+          <div class="input-group">
+            <input type="file" class="form-control" id="inputGroupFile02" />
+            <label class="input-group-text" for="inputGroupFile02">Upload</label>
+          </div>
+        </div>
+        <div class="form-group pt-2">
+          <label for="exampleInputPassword1">Hasil Verifikasi</label>
+          <div class="d-grid gap-2">
+            <button class="btn btn-warning" type="button">Lihat</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.sec2.container {
+  padding: 0;
+  background-color: #239f9f;
+  box-sizing: border-box;
+  height: auto;
+  /* min-height: 100vh; */
+  overflow: hidden;
+  /* padding: 80px 100px 150px; */
+  position: relative;
+  width: 100%;
+  /* margin: 0; */
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,16 +2,19 @@
   <h1>Halo ini dashboard</h1>
   <Jumbotron />
   <SectionSatu />
+  <SectionDua />
 </template>
 
 <script>
 import Jumbotron from "../components/Dashboard/Jumbotron.vue";
 import SectionSatu from "../components/Dashboard/SectionSatu.vue";
+import SectionDua from "../components/Dashboard/SectionDua.vue";
 
 export default {
   components: {
     Jumbotron,
     SectionSatu,
+    SectionDua,
   },
 };
 </script>
