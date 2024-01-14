2f2a90b78aca457ed98d4c98c1138831d3af2a99
Nanda hafiza <yusronnandahafiza@gmail.com>
2024-01-14 23:01:20 +0700

    add section 4

--- src/components/Dashboard/SectionEmpat.vue
+++ src/components/Dashboard/SectionEmpat.vue
@@ -1,3 +1,140 @@
 <template>
-  <h1>section 4</h1>
+  <section class="sec4 container p-5">
+    <div class="row text-center p-5">
+      <h1>Berita Terbaru</h1>
+      <p>Temukan berita terkini tentang ekonomi syariah!</p>
+      <div class="content-tag">
+        <button type="button" class="btn btn-secondary">Trending Topik Sosial Media</button>
+        <button type="button" class="btn btn-secondary">Hukum dan Regulasi Syariah</button>
+        <button type="button" class="btn btn-secondary">Ekonomi Syariah</button>
+        <button type="button" class="btn btn-secondary">Bisnis Syariah</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col p-3">
+        <div class="card mb-3" style="max-width: 540px">
+          <div class="row g-0">
+            <div class="col-md-5 pt-5 ps-4">
+              <img src="../../assets/Berita1.png" alt="" height="80%" />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <p class="card-text"><small class="text-muted">Trending Topik Sosial Media | 3 hari lalu</small></p>
+                <h5 class="card-title">Pengertian SGIE (State of the Global Islamic Economy) dan Manfaatnya</h5>
+                <p class="card-text">Berikut ini pengertian SGIE dan hubungannya dengan Indonesia yang menjadi topik hangat dalam debat Cawapres 2023</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col p-3">
+        <div class="card mb-3" style="max-width: 540px">
+          <div class="row g-0">
+            <div class="col-md-5 pt-5 ps-4 pb-4">
+              <img src="../../assets/Berita3.png" alt="" height="100%" />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <p class="card-text"><small class="text-muted">Bisnis Syariah | 3 hari lalu</small></p>
+                <h5 class="card-title">Tugas Berat Keuangan Syariah RI Capai Pangsa Pasar 50%</h5>
+                <p class="card-text">Saat ini pangsa pasar keuangan syariah masih mencapai 10,94% terhadap total keuangan nasional</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col p-3">
+        <div class="card mb-3" style="max-width: 540px">
+          <div class="row g-0">
+            <div class="col-md-5 pt-5 ps-4">
+              <img src="../../assets/Berita2.png" alt="" height="80%" />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <p class="card-text"><small class="text-muted">Trending Topik Sosial Media | 3 hari lalu</small></p>
+                <h5 class="card-title">Modest Fashion Indonesia Bertengger di Peringkat ke-3 Laporan SGIE 2023/2024</h5>
+                <p class="card-text">Indonesia menjadi kiblat modest fashion dunia pada 2024 berdasarkan laporan SGIE</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col p-3">
+        <div class="card mb-3" style="max-width: 540px">
+          <div class="row g-0">
+            <div class="col-md-5 pt-5 ps-4">
+              <img src="../../assets/Berita6.png" alt="" height="80%" />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <p class="card-text"><small class="text-muted">Ekonomi Syariah | 3 hari lalu</small></p>
+                <h5 class="card-title">Kosistem Ekonomi Syariah Bagian Penting Mendorong Indonesia Emas 2045</h5>
+                <p class="card-text">Berikut ini pengertian SGIE dan hubungannya dengan Indonesia yang menjadi topik hangat dalam debat Cawapres 2023</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col p-3">
+        <div class="card mb-3" style="max-width: 540px">
+          <div class="row g-0">
+            <div class="col-md-5 pt-5 ps-4">
+              <img src="../../assets/Berita4.png" alt="" height="80%" />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <p class="card-text"><small class="text-muted">Trending Topik Sosial Media | 3 hari lalu</small></p>
+                <h5 class="card-title">Buka ISEF-HEI 2023, Wapres Minta Pegiat Ekonomi Syariah Tingkatkan Digitalisasi...</h5>
+                <p class="card-text">Kolaborasi Indonesia Sharia Economic Festival (ISEF)-Halal Expo Indonesia (HEI) merupakan salah satu ikhitiar yang memperkuat mata</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col p-3">
+        <div class="card mb-3" style="max-width: 540px">
+          <div class="row g-0">
+            <div class="col-md-5 pt-5 ps-4">
+              <img src="../../assets/Berita5.png" alt="" height="80%" />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <p class="card-text"><small class="text-muted">Bisnis Syariah | 3 hari lalu</small></p>
+                <h5 class="card-title">Sri Mulyani Dukung Pengembangan Ekonomi Syariah Indonesia</h5>
+                <p class="card-text">Menteri Keuangan Sri Mulyani mengungkapkan, Indonesia menjadi salah satu negara yang memiliki potensi keuangan syariah terbesar di dunia</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="text-center">
+      <button class="btn btn-warning" type="button">Lihat Selengkapnya</button>
+    </div>
+  </section>
 </template>
+
+<style scoped>
+.sec4.container {
+  padding: 0;
+  /* background-color: yellow; */
+  box-sizing: border-box;
+  height: auto;
+  /* min-height: 100vh; */
+  overflow: hidden;
+  /* padding: 80px 100px 150px; */
+  position: relative;
+  width: 100%;
+  /* margin: 0; */
+}
+.sec4.container .content-right {
+  background-color: red;
+}
+.sec4.container .content-left {
+  background-color: blue;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,18 +3,24 @@
   <Jumbotron />
   <SectionSatu />
   <SectionDua />
+  <SectionTiga />
+  <SectionEmpat />
 </template>
 
 <script>
 import Jumbotron from "../components/Dashboard/Jumbotron.vue";
 import SectionSatu from "../components/Dashboard/SectionSatu.vue";
 import SectionDua from "../components/Dashboard/SectionDua.vue";
+import SectionTiga from "../components/Dashboard/SectionTiga.vue";
+import SectionEmpat from "../components/Dashboard/SectionEmpat.vue";
 
 export default {
   components: {
     Jumbotron,
     SectionSatu,
     SectionDua,
+    SectionTiga,
+    SectionEmpat,
   },
 };
 </script>
